21f954f40c1a752916889f916d4e3bc1c610de56
Anastasiya <skvortsova.ak@gmail.com>
2021-10-28 15:31:49 +0300

    footer

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import { Promo } from '../Promo/Promo';
+
+export function Main() {
+  return (
+    <main className="main">
+      <div className="app app_color_pink">
+        <Promo />
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,16 @@
+import promoImg from '../../images/promo.png';
+
+export function Promo() {
+  return (
+    <article className="promo app__section">
+      <section className="app__flex">
+        <div className="promo__text">
+          <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+          <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        </div>
+        <img className="promo__img" src={promoImg} alt="промо картинка" />
+      </section>
+      <button className="promo__button">Узнать больше</button>
+    </article>
+  );
+}
